696eafd0c5db15a52d510c97977ae530ddd330bd
Abilash-IT20132682 <it20132682@my.sliit.lk>
2021-09-22 15:38:30 +0530

    Basic function commit

---
+++ app/src/main/java/com/example/grocery/Constants.java
@@ -0,0 +1,31 @@
+package com.example.grocery;
+
+public class Constants {
+
+    //product categories
+    public static final String[] productCategories = {
+            "Vegetables",
+            "Fruits",
+            "Pharmacy",
+            "Cooking Needs",
+            "Baby Kids",
+            "Biscuits snacks & Chocolates",
+            "Frozen Food",
+            "Breakfast & Diary",
+            "Others"
+    };
+
+    public static final String[] productCategories1 = {
+            "All",
+            "Vegetables",
+            "Fruits",
+            "Pharmacy",
+            "Cooking Needs",
+            "Baby Kids",
+            "Biscuits snacks & Chocolates",
+            "Frozen Food",
+            "Breakfast & Diary",
+            "Others"
+    };
+
+}
---
+++ app/src/main/java/com/example/grocery/activities/ShopDetailsActivity.java
@@ -0,0 +1,402 @@
+package com.example.grocery.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.grocery.Constants;
+import com.example.grocery.R;
+import com.example.grocery.adapters.AdapterCartItem;
+import com.example.grocery.adapters.AdapterProductUser;
+import com.example.grocery.models.ModelCartItem;
+import com.example.grocery.models.ModelProduct;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import p32929.androideasysql_library.Column;
+import p32929.androideasysql_library.EasyDB;
+
+public class ShopDetailsActivity extends AppCompatActivity {
+
+    //declare ui views
+    private ImageView shopIv;
+    private TextView shopNameTv, phoneTv, emailTv, openCloseTv, deliveryFeeTv, addressTv, filteredProductsTv;
+    private ImageButton callBtn, mapBtn, cartBtn, backBtn, filterProductBtn;
+    private EditText searchProductEt;
+    private RecyclerView productsRv;
+
+    private String shopUid;
+    private String myLatitude, myLongitude;
+    private String shopName, shopEmail, shopPhone, shopAddress, shopLatitude, shopLongitude;
+    public String deliveryFee;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ModelProduct> productsList;
+    private AdapterProductUser adapterProductUser;
+
+    //cart
+    private ArrayList<ModelCartItem> cartItemList;
+    private AdapterCartItem adapterCartItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_details);
+
+        //init ui views
+        shopIv = findViewById(R.id.shopIv);
+        shopNameTv = findViewById(R.id.shopNameTv);
+        phoneTv = findViewById(R.id.phoneTv);
+        emailTv = findViewById(R.id.emailTv);
+        openCloseTv = findViewById(R.id.openCloseTv);
+        deliveryFeeTv = findViewById(R.id.deliveryFeeTv);
+        addressTv = findViewById(R.id.addressTv);
+        filteredProductsTv = findViewById(R.id.filteredProductsTv);
+        callBtn = findViewById(R.id.callBtn);
+        mapBtn = findViewById(R.id.mapBtn);
+        cartBtn = findViewById(R.id.cartBtn);
+        backBtn = findViewById(R.id.backBtn);
+        filterProductBtn = findViewById(R.id.filterProductBtn);
+        searchProductEt = findViewById(R.id.searchProductEt);
+        productsRv = findViewById(R.id.productsRv);
+
+        //get uid of the shop from intent
+        shopUid = getIntent().getStringExtra("shopUid");
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loadMyInfo();
+        loadShopDetails();
+        loadShopProducts();
+
+        //each shop have its own  products and orders. So, if user add items to cart and go back and open cart in different shop --> then cart should be different
+        //So, delete  cart data whenever user open this activity
+        deleteCartData();
+
+        //search
+        searchProductEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                try {
+                    adapterProductUser.getFilter().filter(s);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go previous activity
+                onBackPressed();
+            }
+        });
+
+        cartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //show cart dialog
+                showCartDialog();
+
+            }
+        });
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhone();
+            }
+        });
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+
+        filterProductBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ShopDetailsActivity.this);
+                builder.setTitle("Choose Category:")
+                        .setItems(Constants.productCategories1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //get selected item
+                                String selected = Constants.productCategories1[which];
+                                filteredProductsTv.setText(selected);
+                                if(selected.equals("All")){
+                                    //load all
+                                    loadShopProducts();
+                                }else {
+                                    //load filtered
+                                    adapterProductUser.getFilter().filter(selected);
+                                }
+                            }
+                        })
+                        .show();
+            }
+        });
+
+    }
+
+    private void deleteCartData() {
+        EasyDB easyDB = EasyDB.init(this, "ITEMS_DB")
+                .setTableName("ITEMS_TABLE")
+                .addColumn(new Column("Item_Id", new String[]{"text", "unique"}))
+                .addColumn(new Column("Item_PID", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Name", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Price_Each", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Price", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Quantity", new String[]{"text", "not null"}))
+                .doneTableColumn();
+
+        easyDB.deleteAllDataFromTable(); //delete all records from cart
+    }
+
+    public double allTotalPrice = 0.00;
+    //need to access these views in adapter so making public
+    public TextView sTotalTv, dFeeTv, allTotalPriceTv;
+    private void showCartDialog() {
+        //init list
+        cartItemList = new ArrayList<>();
+
+        //inflate cart layout
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_cart, null);
+        //init views
+        TextView shopNameTv = view.findViewById(R.id.shopNameTv);
+        RecyclerView cartItemsRv = view.findViewById(R.id.cartItemsRv);
+        sTotalTv = view.findViewById(R.id.sTotalTv);
+        dFeeTv = view.findViewById(R.id.dFeeTv);
+        allTotalPriceTv = view.findViewById(R.id.totalTv);
+        Button checkoutBtn = view.findViewById(R.id.checkoutBtn);
+
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //set view to dialog
+        builder.setView(view);
+
+        shopNameTv.setText(shopName);
+
+        EasyDB easyDB = EasyDB.init(this, "ITEMS_DB")
+                .setTableName("ITEMS_TABLE")
+                .addColumn(new Column("Item_Id", new String[]{"text", "unique"}))
+                .addColumn(new Column("Item_PID", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Name", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Price_Each", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Price", new String[]{"text", "not null"}))
+                .addColumn(new Column("Item_Quantity", new String[]{"text", "not null"}))
+                .doneTableColumn();
+
+        //get all records from db
+        Cursor res = easyDB.getAllData();
+        while (res.moveToNext()){
+            String id = res.getString(1);
+            String pId = res.getString(2);
+            String name = res.getString(3);
+            String price = res.getString(4);
+            String cost = res.getString(5);
+            String quantity = res.getString(6);
+
+            allTotalPrice = allTotalPrice + Double.parseDouble(cost);
+
+            ModelCartItem modelCartItem = new ModelCartItem(
+                    ""+id,
+                    ""+pId,
+                    ""+name,
+                    ""+price,
+                    ""+cost,
+                    ""+quantity
+            );
+
+            cartItemList.add(modelCartItem);
+        }
+
+        //setup adapter
+        adapterCartItem = new AdapterCartItem(this, cartItemList);
+        //set to recycler view
+        cartItemsRv.setAdapter(adapterCartItem);
+
+        dFeeTv.setText("$"+deliveryFee);
+        sTotalTv.setText("$"+String.format("%.2f", allTotalPrice));
+        allTotalPriceTv.setText("$"+(allTotalPrice + Double.parseDouble(deliveryFee.replace("$",""))));
+
+        //show dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        //reset total price on dialog dismiss
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                allTotalPrice = 0.00;
+            }
+        });
+
+
+
+    }
+
+    private void openMap() {
+        //saddr means source address
+        //daddr means destination address
+        String address = "https://maps.google.com/maps?saddr=" +myLatitude + "," +myLongitude +"&daddr=" +shopLatitude +"," +shopLongitude;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(address));
+        startActivity(intent);
+    }
+
+    private void dialPhone() {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+Uri.encode(shopPhone))));
+        Toast.makeText(this, ""+shopPhone, Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadMyInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds:dataSnapshot.getChildren()){
+                            //get user data
+                            String name = ""+ds.child("name").getValue();
+                            String email = ""+ds.child("email").getValue();
+                            String phone = ""+ds.child("phone").getValue();
+                            String profileImage = ""+ds.child("profileImage").getValue();
+                            String accountType = ""+ds.child("accountType").getValue();
+                            String city = ""+ds.child("city").getValue();
+                            myLatitude = ""+ds.child("latitude").getValue();
+                            myLongitude = ""+ds.child("longitude").getValue();
+
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void loadShopDetails() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(shopUid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //get shop details
+                String name = ""+dataSnapshot.child("name").getValue();
+                shopName = ""+dataSnapshot.child("shopName").getValue();
+                shopEmail = ""+dataSnapshot.child("email").getValue();
+                shopPhone = ""+dataSnapshot.child("phone").getValue();
+                shopLatitude = ""+dataSnapshot.child("latitude").getValue();
+                shopAddress = ""+dataSnapshot.child("address").getValue();
+                shopLongitude = ""+dataSnapshot.child("longitude").getValue();
+                deliveryFee = ""+dataSnapshot.child("deliveryFee").getValue();
+                String profileImage = ""+dataSnapshot.child("profileImage").getValue();
+                String shopOpen = ""+dataSnapshot.child("shopOpen").getValue();
+
+                //set data
+                shopNameTv.setText(shopName);
+                emailTv.setText(shopEmail);
+                deliveryFeeTv.setText("Delivery Fee: $"+deliveryFee);
+                addressTv.setText(shopAddress);
+                phoneTv.setText(shopPhone);
+
+
+                if(shopOpen.equals("true")){
+                    openCloseTv.setText("Open");
+                }else {
+                    openCloseTv.setText("Closed");
+                }
+
+                try {
+                    Picasso.get().load(profileImage).into(shopIv);
+
+                }catch (Exception e){
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadShopProducts() {
+        //init list
+        productsList = new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(shopUid).child("Products")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //clear list before adding items
+                        productsList.clear();
+                        for (DataSnapshot ds: dataSnapshot.getChildren()){
+                            ModelProduct modelProduct = ds.getValue(ModelProduct.class);
+                            productsList.add(modelProduct);
+                        }
+                        //setup adapter
+                        adapterProductUser = new AdapterProductUser(ShopDetailsActivity.this, productsList);
+                        //set adapter
+                        productsRv.setAdapter(adapterProductUser);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+    }
+
+
+
+}
