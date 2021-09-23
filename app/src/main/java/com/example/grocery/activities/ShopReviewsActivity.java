3fff1ac39e26ca00feca4d2cec14df8830086941
Abilash-IT20132682 <it20132682@my.sliit.lk>
2021-09-23 16:07:06 +0530

    Phase Commit

--- app/src/main/java/com/example/grocery/activities/ShopReviewsActivity.java
+++ app/src/main/java/com/example/grocery/activities/ShopReviewsActivity.java
@@ -1,18 +1,42 @@
 package com.example.grocery.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.grocery.R;
 
 public class ShopReviewsActivity extends AppCompatActivity {
 
+    //ui view
+    private ImageButton backBtn;
+    private ImageView profileIv;
+    private TextView shopNameTv, ratingsTv;
+    private RatingBar ratingBar;
+    private RecyclerView reviewsRv;
 
+    private String shopUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_reviews);
+
+        //init ui views
+        backBtn=findViewById(R.id.backBtn);
+        profileIv=findViewById(R.id.profileIv);
+        shopNameTv=findViewById(R.id.shopNameTv);
+        ratingBar=findViewById(R.id.ratingBar);
+        ratingsTv=findViewById(R.id.ratingsTv);
+        reviewsRv=findViewById(R.id.reviewsRv);
+
+        //get shop uid from intent
+        shopUid = getIntent().getStringExtra("shopUid");
+
     }
 }
