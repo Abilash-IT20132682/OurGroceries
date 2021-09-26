5b73bed78f6f8f9acf1aa8950afc84540ec7346d
Kunekanth <it20069704@my.sliit.lk>
2021-09-26 15:10:41 +0530

    function commit

--- app/src/main/java/com/example/grocery/activities/ShopDetailsActivity.java
+++ app/src/main/java/com/example/grocery/activities/ShopDetailsActivity.java
@@ -168,7 +168,7 @@ public class ShopDetailsActivity extends AppCompatActivity {
             }
         });
 
-        //Handle click of the back button
+        //Handle click of the back button....
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
